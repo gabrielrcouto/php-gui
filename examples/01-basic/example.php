9df92a6c4b5ce0a4230e841850b84f49a21402dc
Gabriel Rodrigues Couto <gabrielrcouto@gmail.com>
2016-04-06 23:06:32 -0300

    New basic example

--- examples/01-basic/example.php
+++ examples/01-basic/example.php
@@ -5,35 +5,77 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Gui\Application;
 use Gui\Components\Button;
 use Gui\Components\InputText;
+use Gui\Components\Label;
 use Gui\Components\Shape;
 
 $application = new Application();
 
 $application->on('start', function() use ($application) {
+    $label = (new Label())
+        ->setFontSize(20)
+        ->setLeft(90)
+        ->setText('Basic Example')
+        ->setTop(10);
+
     $button = (new Button())
         ->setCounter(1)
-        ->setLeft(100)
-        ->setTop(100)
-        ->setValue('Dont click');
+        ->setLeft(10)
+        ->setTop(80)
+        ->setValue('Hey, I\'m a button! Please, don\'t click me')
+        ->setWidth(300);
 
     $input = (new InputText())
         ->setLeft(10)
-        ->setValue('Made with PHP S2!')
+        ->setValue('This input was made with PHP S2!')
         ->setTop(50)
-        ->setWidth(290);
+        ->setWidth(300);
 
-    $button->on('click', function() use ($button, $input) {
-        $button->setValue('Ouch ' . $button->getCounter() . 'x');
-        $button->setCounter($button->getCounter() + 1);
+    $phrases = [
+        'Ouch, I said "Don\'t click me"',
+        'Ok, stop',
+        'Please!',
+        'What you doing?',
+        'Nice, I know, you have a mouse',
+        'Yep, with the mouse you can click',
+        'Formating C:',
+        'Yeah, was a joke',
+        'Do you know about PHP-SP?',
+        'Drink coke',
+        'Click to skip this ad',
+    ];
 
-        if ($button->getCounter() >= 20) {
-            $text = 'Please, stop! You already clicked ' . preg_replace('/[^0-9]/', '', $button->getValue()) . ' times';
-            $input->setValue($text);
-        }
+    $button->on('click', function() use ($button, $input, $phrases) {
+        $counter = $button->getCounter();
+        $button->setCounter(++$counter);
 
-        if ($button->getCounter() == 30) {
+        if ($counter < 10) {
+            $input->setValue($phrases[$counter - 1]);
+        } else if ($counter < 20) {
+            $input->setValue('Please, stop! You already clicked ' . $counter . ' times');
+        } else if ($counter == 20) {
+            $input->setValue('Hehe, click again!');
             $button->setVisible(false);
         }
+
+        if ($counter < 10) {
+            $counter = '0' . $counter;
+        }
+
+        $color = $counter * (1.0 / 6);
+
+        $r = (int)(3 * sin($color                 ) + 3);
+        $g = (int)(3 * sin($color + 2 * (M_PI / 3)) + 3);
+        $b = (int)(3 * sin($color + 4 * (M_PI / 3)) + 3);
+
+        $color = '#' . dechex($r) . dechex($g) . dechex($b);
+
+        $shape = (new Shape())
+            ->setBackgroundColor($color)
+            ->setBorderColor($color)
+            ->setHeight($counter * 5)
+            ->setLeft($counter * 10)
+            ->setTop(120)
+            ->setWidth($counter * 5);
     });
 });
 
