fd1882d467af4fcfff7adfeba8ab59ef12472f37
Leonn Leite <leonnleite@gmail.com>
2016-10-21 11:36:42 +0000

    fix cs s

--- test/Components/RadioTest.php
+++ test/Components/RadioTest.php
@@ -1,25 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: leonnleite
- * Date: 21/10/16
- * Time: 02:16
- */
 
 namespace Test\Components;
-
 
 use Gui\Application;
 use Gui\Components\Option;
 use Gui\Components\Radio;
-use Gui\Components\Window;
 
 class RadioTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testSetOptions()
     {
-
         $radio = new Radio([], null, new Application());
 
         $this->assertSame($radio, $radio->setOptions([new Option('foo', 1)]));
